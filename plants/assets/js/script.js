e53abd7c623a900c5f733730afecee183c5caff4
slawkir <slawkir1984@gmail.com>
2023-02-05 12:23:54 +0400

    feat function blur for planting and lawn

--- plants/assets/js/script.js
+++ plants/assets/js/script.js
@@ -4,11 +4,27 @@ let buttonPlanting = document.querySelectorAll('.service__btn')[2];
 
 let allCards = document.querySelectorAll('.service__item');
 
+
+
 let cardsWithoutGardens = [];
 for (let i = 0; i < allCards.length; i++) {
   if (!allCards[i].classList.contains('card-gardens')) { 
     cardsWithoutGardens.push(allCards[i]);
   }
+}
+let cardsWithoutPlanting = [];
+for (let i = 0; i < allCards.length; i++) {
+  if (!allCards[i].classList.contains('card-planting')) { 
+    cardsWithoutPlanting.push(allCards[i]);
+  }
+}
+
+let cardWithoutLawn = [];
+for (let i = 0; i < allCards.length; i++) {
+  if (!allCards[i].classList.contains('card-lawn')) { 
+    cardWithoutLawn.push(allCards[i]);
+  }
+  
 }
 
 buttonGardens.addEventListener('click', function () {
@@ -16,6 +32,26 @@ buttonGardens.addEventListener('click', function () {
     if (element.classList.contains('blur')) {
       element.classList.remove('blur');
     } else { 
+      element.classList.add('blur');
+    }
+  });
+});
+
+buttonPlanting.addEventListener('click', function () {
+  cardsWithoutPlanting.forEach(element => {
+    if (element.classList.contains('blur')) {
+      element.classList.remove('blur');
+    } else {
+      element.classList.add('blur');
+    }
+  });
+});
+
+buttonLawn.addEventListener('click', function () {
+  cardWithoutLawn.forEach(element => {
+    if (element.classList.contains('blur')) {
+      element.classList.remove('blur');
+    } else {
       element.classList.add('blur');
     }
   });
